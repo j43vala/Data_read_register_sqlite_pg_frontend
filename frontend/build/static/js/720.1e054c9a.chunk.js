861d6826661973a1cf47ad468db325a07ead171e
j43vala <143093071+j43vala@users.noreply.github.com>
2023-11-25 12:01:14 +0530

    BUILD FOLDER ADDED

---
+++ frontend/build/static/js/720.1e054c9a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstarter_bt5=self.webpackChunkstarter_bt5||[]).push([[720],{1720:(s,e,t)=>{t.r(e),t.d(e,{default:()=>a});t(2791);var r=t(184);const a=()=>(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Modbus"})})}}]);
+//# sourceMappingURL=720.1e054c9a.chunk.js.map
