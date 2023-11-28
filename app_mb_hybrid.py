55e44c12a1bfc88221dd687ed23a3e170fc68333
j43vala <143093071+j43vala@users.noreply.github.com>
2023-11-28 18:50:42 +0530

    final hybrid

--- app_mb_hybrid.py
+++ app_mb_hybrid.py
@@ -1,83 +1,211 @@
-#------------------------------------------------------- 
-#   SQLITE  +  SPARKPLUG_B 
-#------------------------------------------------------- 
+# #------------------------------------------------------- 
+# #   SQLITE  +  SPARKPLUG_B 
+# #------------------------------------------------------- 
  
+# import time
+# import socket 
+# from config import config 
+# from config.db import get_sqlite_session 
+# from modbus_final import read_modbus_data, initialize_modbus_client
+# from sqlite_final import create_dynamic_models 
+# from spb import init_spb_device
+
+# def main():
+#     try:
+#         # check if the configuration is available or not 
+#         if config is not None:
+#             #Get the communication port and device information from the configuration 
+#             com_port = config["modbus"]["port"]
+#             print('com_port: ', com_port)
+#             devices = config["devices"]
+#             client = initialize_modbus_client()
+#             client.connect()
+
+#             #hostname = os.uname()[1]           # for linux 
+#             hostname = socket.gethostname()     # for windows
+
+#             #Initialize Sqlite Dynamic Models 
+#             create_dynamic_models(devices, hostname)
+
+#             session = get_sqlite_session()
+
+#             #sparkplug_b configureation 
+#             group_name = config.get("spb_parameter").get("group_id")
+#             edge_node_id = config.get("spb_parameter").get("edge_node_id")
+
+#             #spb_dev initilized
+#             for device_dict in config["devices"]:
+#                 init_spb_device(group_name, edge_node_id, device_dict)
+
+#             while True:
+#                 time.sleep(3)
+#                 for device_dict in devices:
+#                     try:
+#                         model = device_dict["model"]
+#                         record = model()
+#                         slave_id = device_dict.get("slave_id")
+#                         spb_device = device_dict["spb_device"]
+
+#                         for parameter in device_dict["parameters"]:
+#                             try:
+#                                 parameter_name = parameter.get("parameter_name")
+#                                 reg_no = parameter.get("address")
+#                                 reg_data_type = parameter.get("data_type")
+
+#                                 if parameter_name is not None:
+#                                     data = read_modbus_data(client, slave_id, reg_no, reg_data_type)
+#                                     setattr(record, parameter_name, data)
+#                                     spb_device.data.set_value(parameter_name, data)
+#                                     print(f"updated '{parameter_name}' with value: {data}")
+#                                 else:
+#                                     print(f"Attribute name is missing in the specification for parameter {parameter}")
+
+#                             except Exception as e:
+#                                 import traceback
+#                                 traceback.print_exc()
+#                                 print(f"An error occurred processing parameter {parameter}: {e}")
+
+#                         session.add(record)
+#                         session.commit()
+#                         spb_device.publish_data()  # Fixed typo here
+#                         device_name = device_dict["device_name"]
+#                         print(f"Record Committed Successfully for device: '{device_name}' \n\n")
+
+#                     except Exception as e:
+#                         print(f"An error occurred: {e}")
+#                         break
+
+#     except KeyboardInterrupt:
+#         client.close()
+#         print("Exiting the loop...........")
+
+# if __name__ == "__main__":  # Fixed typo here
+#     main()
+
+
+
 import time
-import socket 
-from config import config 
-from config.db import get_sqlite_session 
+import socket
+import datetime
+from config import config
+from config.db import get_sqlite_session
 from modbus_final import read_modbus_data, initialize_modbus_client
-from sqlite_final import create_dynamic_models 
+from sqlite_final import create_dynamic_models
 from spb import init_spb_device
 
 def main():
+    last_check_time = datetime.datetime.now()
+    retention_interval = 10  # Set the retention interval in seconds (set to 0 for infinite storage)
+
     try:
-        # check if the configuration is available or not 
+        # check if the configuration is available or not
         if config is not None:
-            #Get the communication port and device information from the configuration 
+            # Get the communication port and device information from the configuration
             com_port = config["modbus"]["port"]
             print('com_port: ', com_port)
             devices = config["devices"]
             client = initialize_modbus_client()
             client.connect()
 
-            #hostname = os.uname()[1]           # for linux 
+            # hostname = os.uname()[1]           # for linux
             hostname = socket.gethostname()     # for windows
 
-            #Initialize Sqlite Dynamic Models 
+            # Initialize Sqlite Dynamic Models
             create_dynamic_models(devices, hostname)
 
             session = get_sqlite_session()
 
-            #sparkplug_b configureation 
+            # sparkplug_b configuration
             group_name = config.get("spb_parameter").get("group_id")
             edge_node_id = config.get("spb_parameter").get("edge_node_id")
 
-            #spb_dev initilized
+            # spb_dev initialized
+            success = None  # Initialize success variable for the entire process
+
             for device_dict in config["devices"]:
                 init_spb_device(group_name, edge_node_id, device_dict)
 
             while True:
-                time.sleep(3)
+                time.sleep(1.5)
+
                 for device_dict in devices:
-                    try:
-                        model = device_dict["model"]
-                        record = model()
-                        slave_id = device_dict.get("slave_id")
-                        spb_device = device_dict["spb_device"]
+                    model = device_dict["model"]
+                    record = model()
+                    slave_id = device_dict.get("slave_id")
+                    spb_device = device_dict["spb_device"]
 
-                        for parameter in device_dict["parameters"]:
+                    for parameter in device_dict["parameters"]:
+                        parameter_name = parameter.get("parameter_name")
+                        reg_no = parameter.get("address")
+                        reg_data_type = parameter.get("data_type")
+
+                        if parameter_name is not None:
                             try:
-                                parameter_name = parameter.get("parameter_name")
-                                reg_no = parameter.get("address")
-                                reg_data_type = parameter.get("data_type")
-
-                                if parameter_name is not None:
-                                    data = read_modbus_data(client, slave_id, reg_no, reg_data_type)
-                                    setattr(record, parameter_name, data)
-                                    spb_device.data.set_value(parameter_name, data)
-                                    print(f"updated '{parameter_name}' with value: {data}")
-                                else:
-                                    print(f"Attribute name is missing in the specification for parameter {parameter}")
-
+                                data = read_modbus_data(client, slave_id, reg_no, reg_data_type)
+                                setattr(record, parameter_name, data)
+                                spb_device.data.set_value(parameter_name, data)
+                                print(f"updated '{parameter_name}' with value: {data}")
                             except Exception as e:
-                                import traceback
-                                traceback.print_exc()
-                                print(f"An error occurred processing parameter {parameter}: {e}")
+                                success = False
+                                print(f"An error occurred reading Modbus data for {parameter}: {e}")
+                        else:
+                            print(f"Attribute name is missing in the specification for parameter {parameter}")
 
-                        session.add(record)
-                        session.commit()
-                        spb_device.publish_data()  # Fixed typo here
-                        device_name = device_dict["device_name"]
-                        print(f"Record Committed Successfully for device: '{device_name}' \n\n")
+                    session.add(record)
+                    session.commit()
+
+                    # Attempt to publish data to Sparkplug B
+                    try:
+                        success = spb_device.publish_data()
+                        print('success: ', success)
+
+                        # Data retention logic based on the success of publishing
+                        if retention_interval == 0:
+                            # Infinite storage, no need for retention checks
+                            pass
+                        else:
+                            retention_period = datetime.datetime.utcnow() - datetime.timedelta(seconds = 40)
+                            elapsed_time = datetime.datetime.now() - last_check_time
+
+                            if success and elapsed_time.total_seconds() >= retention_interval:
+                                last_check_time = datetime.datetime.now()
+                                print("Check 40 sec...")
+
+                                # Assuming you have a timestamp in your data, modify the following code accordingly
+                                records_to_delete = session.query(model).filter(model.timestamp < retention_period).all()
+
+                                for record in records_to_delete:
+                                    session.delete(record)
+
+                                session.commit()
+
+                                device_name = device_dict["device_name"]
+                                print(f"Record committed for device: '{device_name}'\n\n")
+                            elif not success:
+                                retention_period_failure = datetime.datetime.utcnow() - datetime.timedelta(seconds = 50)
+                                elapsed_time = datetime.datetime.now() - last_check_time
+                                if elapsed_time.total_seconds() >= retention_interval:
+                                    last_check_time = datetime.datetime.now()
+                                    print("Check  for 50 sec failure case...")
+
+                                    # Assuming you have a timestamp in your data, modify the following code accordingly
+                                    records_to_delete_failure = session.query(model).filter(model.timestamp < retention_period_failure).all()
+
+                                    for record in records_to_delete_failure:
+                                        session.delete(record)
+
+                                    session.commit()
+
+                                    device_name = device_dict["device_name"]
+                                    print(f"Record committed for device (failure case): '{device_name}'\n\n")
 
                     except Exception as e:
-                        print(f"An error occurred: {e}")
-                        break
+                        success = False
+                        print(f"Failed to publish data to Sparkplug B: {e}")
 
     except KeyboardInterrupt:
         client.close()
         print("Exiting the loop...........")
 
-if __name__ == "__main__":  # Fixed typo here
+if __name__ == "__main__":
     main()
