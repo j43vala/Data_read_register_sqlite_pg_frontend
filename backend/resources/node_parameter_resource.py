63a1d8f85420c59f29eafe851fb188672e617dc2
rutikdev0906 <rutikparmar445@gmail.com>
2023-12-20 09:57:50 +0530

    update mqtt

--- backend/resources/node_parameter_resource.py
+++ backend/resources/node_parameter_resource.py
@@ -39,7 +39,11 @@ def create_default_node_parameters():
             "name": "mqtt",
             "value": {
                 "broker_host": "broker.hivemq.com",
-                "broker_port": 1883
+                "broker_port": 1883,
+                "qos":00,
+                "qos_options": [0,1,2],
+                "client_id": "",
+                "client_password": "",
             }
         },
         {
